3fb30039805817aff1399fc16bc2a05226f74df5
monsterxcn <monsterxcn@users.noreply.github.com>
2020-05-15 16:11:41 +0000

    Automated deployment: Fri May 15 16:11:41 UTC 2020 5058e9eea28d8c8c6fb39b20433190285925c7f0

--- includes/head.php
+++ includes/head.php
@@ -64,7 +64,7 @@ if (isset($_POST['void_action'])) {
     <?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&description=&'); ?>
 
     <link rel="stylesheet" href="<?php echo $assetsUrl.'/bundle-2457c24936.css'; ?>">
-    <link rel="stylesheet" href="<?php echo $assetsUrl.'/VOID-c4bc1967d3.css'; ?>">
+    <link rel="stylesheet" href="<?php echo $assetsUrl.'/VOID-9082fcd1a4.css'; ?>">
 
     <?php if($setting['VOIDPlugin'] == 'true' && Helper::options()->plugin('VOID')->exswitch == 'true'): ?>
     <link rel="stylesheet" href="<?php Helper::options()->pluginUrl('/VOID/pages/exsearch.css'); ?>">
